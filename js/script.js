b0b05ca228433b9ffb7bc058c5ab5b0e34c3add0
MiDo <mohammed.mido9719@gmail.com>
2019-08-09 17:39:47 +0200

    minor fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
-    $("body").niceScroll({
-        cursorcolor:"#ff9b51",
-        cursorwidth:"6px",
-        cursorborder:'1px solid #ac2214'
+    // $("body").niceScroll({
+    //     cursorcolor:"#ff9b51",
+    //     cursorwidth:"6px",
+    //     cursorborder:'1px solid #ac2214'
+    // });
+
+
+    // header
+    $('body').css("padding-top",$('.header .navbar').innerHeight());
+    $(window).scroll(function() {
+        $(this).scrollTop() >= 600 ? $('.header .navbar.navbar-inverse.navbar-fixed-top').css('opacity','0.75') : $('.header .navbar.navbar-inverse.navbar-fixed-top').css('opacity','1');
     });
+
+        // Navigation Links
+        for(let i =1; i<6; i++) {
+            $('.header nav .collapse ul > li a').eq(i).on('click', function() {
+                $('html').animate({
+                    scrollTop: $($(this).data('section')).offset().top - parseInt($($(this).data('section')).css('padding-top'))
+                },1000);
+                $(this).parent().addClass('active').siblings().removeClass('active');
+            });
+        }
 
     // Inputs placeholder
     var placeholder;
@@ -17,16 +34,6 @@ $(document).ready(function() {
             $(this).attr('placeholder', placeholder);
         }
     });
-
-    // Navigation Links
-    for(let i =1; i<6; i++) {
-        $('.header nav .collapse ul > li a').eq(i).on('click', function() {
-            $('html').animate({
-                scrollTop: $($(this).data('section')).offset().top
-            },1000);
-            $(this).parent().addClass('active').siblings().removeClass('active');
-        });
-    }
 
     // Footer
         // To Top button
